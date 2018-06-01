7f221daf779efd742831968fab18d2fe065b46d4
Ashleigh <ashleighc207@gmail.com>
2018-06-01 19:52:15 -0400

    Fix sw registration

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
-if(navigator.serviceWorker){
-	navigator.serviceWorker.register('/sw.js').then(function(reg){
-		console.log('Registration Successful');
-	}).catch(function(err){
-		console.log('There was an error:', err);
-	});
-}
-
-
+navigator.serviceWorker.register('/sw.js').then(function(reg) {
+	console.log("Registration Successful..");
+}).catch(function(err){
+	console.log("Error:", err);
+});
