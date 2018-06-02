f0d16551c9cc8a01185a230ea2d33518994a13c1
Ashleigh <ashleighc207@gmail.com>
2018-06-01 20:54:26 -0400

    Add offline capability

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-let staticCacheName = 'foodie-cache-v1';
+var staticCacheName = 'foodie-cache-v1';
 
 self.addEventListener('install', function (event) {
 	event.waitUntil(caches.open(staticCacheName).then(function(cache) {
 		return cache.addAll([
+			'/', 
 			'/index.html', 
 			'js/main.js', 
 			'js/index.js', 
@@ -24,9 +25,10 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			if(response) return response;
+			return fetch(event.request);
+		})
+	);
 });
