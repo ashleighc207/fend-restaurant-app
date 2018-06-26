a225b0b418d1927e304bac789294e3ca760364bb
Ashleigh <ashleighc207@gmail.com>
2018-06-26 11:42:12 -0400

    Upload service worker

---
+++ sw.js
@@ -0,0 +1,35 @@
+var staticCacheName = 'foodie-cache-v3';
+
+self.addEventListener('install', function (event) {
+	event.waitUntil(caches.open(staticCacheName).then(function (cache) {
+		return cache.addAll([
+			'/',
+			'/index.html',
+			'js/main.js',
+			'js/index.js',
+			'css/styles.css',
+			'img/sushi-king.jpg',
+			'img/ginos.jpg',
+			'img/honey-pig.jpg',
+			'img/royal-taj.jpg',
+			'img/pizza-sauce.jpg',
+			'img/pho-pasteur.jpg',
+			'img/spice-dice.jpg',
+			'img/birroteca.jpg',
+			'img/andy-nelsons.jpg',
+			'img/thb.jpg',
+			'img/favicon.png'
+		]).catch(function (err) {
+			console.log("Error: ", err);
+		});
+	}));
+});
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			if (response) return response;
+			return fetch(event.request);
+		})
+	);
+});
