27af6b547fb452b98a505c6205f034aea6e6f55a
Ashleigh <ashleighc207@gmail.com>
2018-06-26 11:26:56 -0400

    Update file path for images

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -158,7 +158,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`../img/${restaurant.photograph}`);
   }
 
   /**
