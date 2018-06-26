c654d5dc944ccb589d792a9c3fa366d5022ec29f
Ashleigh <ashleighc207@gmail.com>
2018-06-26 11:43:34 -0400

    Update file path

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-navigator.serviceWorker.register('/sw.js').then(function(reg) {
+navigator.serviceWorker.register('./sw.js').then(function(reg) {
 	console.log("Registration Successful..");
 }).catch(function(err){
 	console.log("Error:", err);
