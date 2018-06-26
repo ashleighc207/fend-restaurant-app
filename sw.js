9fea03e70fc63a468985f6e0b271061cc01d1300
Ashleigh <ashleighc207@gmail.com>
2018-06-26 11:58:42 -0400

    Update file paths

--- sw.js
+++ sw.js
@@ -3,24 +3,23 @@ var staticCacheName = 'foodie-cache-v5';
 self.addEventListener('install', function (event) {
 	event.waitUntil(caches.open(staticCacheName).then(function (cache) {
 		return cache.addAll([
-			'/',
 			'/fend-restaurant-app/',
 			'/fend-restaurant-app/index.html',
-			'/index.html',
-			'/js/main.js',
-			'/js/index.js',
-			'/css/styles.css',
-			'/img/sushi-king.jpg',
-			'/img/ginos.jpg',
-			'/img/honey-pig.jpg',
-			'/img/royal-taj.jpg',
-			'/img/pizza-sauce.jpg',
-			'/img/pho-pasteur.jpg',
-			'/img/spice-dice.jpg',
-			'/img/birroteca.jpg',
-			'/img/andy-nelsons.jpg',
-			'/img/thb.jpg',
-			'/img/favicon.png'
+			'/fend-restaurant-app/index.html',
+			'/fend-restaurant-app/js/main.js',
+			'/fend-restaurant-app/js/index.js',
+			'/fend-restaurant-app/css/styles.css',
+			'/fend-restaurant-app/img/sushi-king.jpg',
+			'/fend-restaurant-app/img/ginos.jpg',
+			'/fend-restaurant-app/img/honey-pig.jpg',
+			'/fend-restaurant-app/img/royal-taj.jpg',
+			'/fend-restaurant-app/img/pizza-sauce.jpg',
+			'/fend-restaurant-app/img/pho-pasteur.jpg',
+			'/fend-restaurant-app/img/spice-dice.jpg',
+			'/fend-restaurant-app/img/birroteca.jpg',
+			'/fend-restaurant-app/img/andy-nelsons.jpg',
+			'/fend-restaurant-app/img/thb.jpg',
+			'/fend-restaurant-app/img/favicon.png'
 		]).catch(function (err) {
 			console.log("Error: ", err);
 		});
