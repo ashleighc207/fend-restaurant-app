438f38acb7403ade6b801692ebac526258e1ff4f
Ashleigh <ashleighc207@gmail.com>
2018-06-26 11:14:20 -0400

    Update url for json

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8080; // Change this to your server port
-    return `https://github.com/ashleighc207/fend-restaurant-app/blob/master/data/restaurants.json`;
+    return `https://ashleighc207.github.io/fend-restaurant-app/data/restaurants.json`;
   }
 
   /**
