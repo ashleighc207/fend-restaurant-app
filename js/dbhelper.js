831aba2c585bd51cc60ce363e0ddc4295dfb62a1
Ashleigh <ashleighc207@gmail.com>
2018-06-26 11:09:51 -0400

    Test db change for github pages

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8080; // Change this to your server port
-    return `/data/restaurants.json`;
+    return `https://github.com/ashleighc207/fend-restaurant-app/blob/master/data/restaurants.json`;
   }
 
   /**
